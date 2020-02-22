baa82ec29bc333ccbe72b0267d7e183ccc47c02a
Greg Wuller <greg@afofo.com>
2020-02-22 15:12:18 -0800

    send toggle state on script init event

--- lib/device/ui.lua
+++ lib/device/ui.lua
@@ -17,6 +17,11 @@ function Toggle:process(event, output, state)
     if self.action then
       self.action(self.state)
     end
+  elseif sky.is_type(event, sky.SCRIPT_INIT_EVENT) then
+    -- call action with default initial state
+    if self.action then
+      self.action(self.state)
+    end
   end
   output(event)
 end
