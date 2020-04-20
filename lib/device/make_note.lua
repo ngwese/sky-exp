a2cf3315bd0c9d239ee966b6baf1552b327dd4ce
Greg Wuller <greg@afofo.com>
2020-04-20 01:27:28 -0700

    make note can set duration

--- lib/device/make_note.lua
+++ lib/device/make_note.lua
@@ -19,16 +19,36 @@ function MakeNote:device_removed(chain)
 end
 
 function MakeNote:process(event, output, state)
-  if event.from == self then  -- previously scheduled
+  -- just output it if we previously scheduled it
+  if event.from == self then
     output(event)
-  elseif sky.is_type(event, sky.types.NOTE_ON) and event.duration ~= nil then
-    output(event)
-    local note_off = sky.mk_note_off(event.note, 0, event.ch)
-    note_off.from = self
-    self._scheduler:sync(event.duration, note_off)
-  else
-    output(event)
+    return
   end
+
+  -- filter out any note offs if we are going to apply a duration
+  if sky.is_type(event, sky.types.NOTE_OFF) and self.duration then
+    return
+  end
+
+  -- set duration if need be and schedule
+  if sky.is_type(event, sky.types.NOTE_ON) then
+    -- stamp duration if there isn't one
+    if event.duration == nil then
+      event.duration = self.duration
+    end
+
+    output(event)
+
+    if event.duration ~= nil then
+      local note_off = sky.mk_note_off(event.note, 0, event.ch)
+      note_off.from = self
+      self._scheduler:sync(event.duration, note_off)
+    end
+    return
+  end
+
+  -- something else or a NOTE_OFF because we aren't setting duration
+  output(event)
 end
 
 return {
