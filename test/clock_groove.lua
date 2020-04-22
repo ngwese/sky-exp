7df68f68e8314382149524d654565674a59f4455
Greg Wuller <greg@afofo.com>
2020-04-21 23:37:15 -0700

    groove/clock test
    something unexpected is happening with clock.sync.
    a 3:2 test gives expected results after the
    first beat/bar is reached but before that it isn't
    clear why the intervals are what they are

--- test/clock_groove.lua
+++ test/clock_groove.lua
@@ -1,28 +1,56 @@
 include('sky/lib/prelude')
 sky.use('sky/lib/core/groove')
-sky.use('sky/lib/device/arp')
 sky.use('sky/lib/io/clock')
+sky.use('sky/lib/device/make_note')
 sky.use('sky/lib/engine/polysub')
 
 chain = sky.Chain{
-  sky.Held{},      -- track held notes, emit on change
-  sky.Pattern{},   -- generate pattern when held notes change
-  sky.Arp{},       -- generate notes from pattern
-  sky.Logger{},
-  sky.PolySub{}
+  sky.Logger{ show_beats = true },
+  --sky.PolySub{}
+  sky.Output{ name = "UM-ONE" },
 }
 
-clk = sky.Clock{ chain = chain }
-in1 = sky.Input{ name = "AXIS-64", chain = chain }
+local build_clock = function(note_num, to_chain, ch)
+  return sky.Clock{
+    chain = sky.Chain{
+      function(event, output)
+        if sky.is_clock(event) then
+          output(event)
+          output(sky.mk_note_on(note_num, 100, ch))
+        end
+      end,
+      sky.MakeNote{ duration = 1/16 }, -- FIXME: this ends on next 16ths inverval boundry not 1/16th in length
+      sky.Forward(to_chain),
+    }
+  }
+end
 
-g1 = sky.Groove.new{ 1/4, 1/4, 1/8, 3/8 }
+clk1 = build_clock(68, chain, 1)
+clk2 = build_clock(40, chain, 2)
+
+g1 = sky.Groove.straight(3)
+g2 = sky.Groove.straight(2)
+
+function start()
+  clk1.interval = 1/3
+  clk2.interval = 1/2
+  clk1:start_sync(g1)
+  clk2:start_sync(g2)
+  --clk1:start()
+  --clk2:start()
+end
+
+function stop()
+  clk1:stop()
+  clk2:stop()
+end
 
 function init()
   -- polysub
-  params:set('ampatk', 0.01)
-  params:set('amprel', 0.05)
+  --params:set('ampatk', 0.01)
+  --params:set('amprel', 0.05)
 
-  clk:play_sync(g1)
+  --start()
 end
 
 function redraw()
@@ -31,5 +59,5 @@ function redraw()
 end
 
 function cleanup()
-  clk:stop()
+  stop()
 end
